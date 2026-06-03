b1c4fddd9e088d9082ca2a396f301d482223837d
Juan Carlos Mena Varas <ext_jmena@Falabella.com>
2026-06-03 11:16:10 -0400

    Cambio de nombre a Eleodoro El Grande

--- app_simple/app.py
+++ app_simple/app.py
@@ -40,22 +40,37 @@ def init_db():
     conn.close()
 
 def cargar_datos_mock(conn):
-    bebidas = ["Coca-Cola", "Pepsi", "Fanta", "Sprite", "Agua Mineral", "Agua con Gas", "Red Bull", "Monster", "Cerveza Cristal", "Corona", "Heineken", "Vino Cabernet", "Whisky", "Ron", "Vodka", "Pisco"]
+    licores = ["Pisco Alto del Carmen", "Pisco Mistral", "Pisco Capel", "Whisky Johnnie Walker Red", "Whisky Johnnie Walker Black", "Whisky Chivas Regal", "Ron Bacardi Añejo", "Ron Havana Club", "Ron Flor de Caña", "Vodka Absolut", "Vodka Smirnoff", "Tequila Jose Cuervo", "Gin Tanqueray", "Gin Beefeater", "Baileys", "Jägermeister", "Fernet Branca"]
+    vinos = ["Vino Casillero del Diablo Cabernet", "Vino Gato Negro Merlot", "Vino Concha y Toro Sauvignon Blanc", "Vino Santa Helena Cármenère", "Vino Misiones de Rengo", "Vino Tarapacá Gran Reserva", "Vino 120 Santa Rita"]
+    cervezas = ["Cerveza Cristal", "Cerveza Escudo", "Cerveza Royal Guard", "Cerveza Corona", "Cerveza Heineken", "Cerveza Stella Artois", "Cerveza Becker", "Cerveza Kunstmann Torobayo", "Cerveza Austral Calafate"]
+    bebidas = ["Coca-Cola", "Coca-Cola Zero", "Pepsi", "Fanta", "Sprite", "Kem Piña", "Bilz", "Pap", "Crush", "Canada Dry Ginger Ale", "Agua Mineral Cachantun", "Agua Benedictino", "Red Bull", "Monster Energy", "Jugo Watts Durazno", "Jugo Andina Naranja", "Gatorade Blue"]
+
+    todas_las_bebidas = licores + vinos + cervezas + bebidas
     tipos_venta = ["Unidad", "Pack", "Caja", "Jaba"]
 
     def generar_sku(idp): return f"BEB-{idp:05d}"
     
-    for i in range(1, 21): 
+    for i in range(1, 1001): 
+        nombre_base = random.choice(todas_las_bebidas)
         tipo = random.choice(tipos_venta)
-        if tipo == "Unidad": precio = random.randint(800, 3000)
-        elif tipo == "Pack": precio = random.randint(4000, 12000)
-        elif tipo == "Caja": precio = random.randint(12000, 40000)
-        else: precio = random.randint(20000, 60000)
+        
+        if nombre_base in licores: precio_base = random.randint(6000, 25000)
+        elif nombre_base in vinos: precio_base = random.randint(3000, 15000)
+        elif nombre_base in cervezas: precio_base = random.randint(1000, 2500)
+        else: precio_base = random.randint(800, 2500)
+
+        if tipo == "Unidad": precio = precio_base
+        elif tipo == "Pack": precio = precio_base * random.randint(4, 6)
+        elif tipo == "Caja": precio = precio_base * random.randint(12, 24)
+        else: precio = precio_base * random.randint(6, 12)
+
+        tamano = random.choice(["350cc", "500cc", "1L", "1.5L", "2L", "3L", "750ml"])
+        nombre_final = f"{nombre_base} {tamano}"
         
         conn.execute('''
             INSERT INTO productos (sku, nombre, tipo_venta, precio, stock, pasillo, posicion)
             VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (generar_sku(i), f"{random.choice(bebidas)} {random.randint(250,1500)}ml", tipo, precio, random.randint(10, 500), random.choice(string.ascii_uppercase), random.randint(1, 100)))
+        ''', (generar_sku(i), nombre_final, tipo, precio, random.randint(0, 1000), random.choice(string.ascii_uppercase), random.randint(1, 100)))
     conn.commit()
 
 # ======================
